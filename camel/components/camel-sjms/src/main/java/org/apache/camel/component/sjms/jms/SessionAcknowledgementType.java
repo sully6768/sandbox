393a2bca45ff239a54b3809c686989579452bd99
Scott England-Sullivan <scottes@fusesource.com>
2012-04-26 19:15:43 -0400

    Javadoc update

--- camel/components/camel-sjms/src/main/java/org/apache/camel/component/sjms/jms/SessionAcknowledgementType.java
+++ camel/components/camel-sjms/src/main/java/org/apache/camel/component/sjms/jms/SessionAcknowledgementType.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2012 FuseSource
- *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -16,7 +14,7 @@
 package org.apache.camel.component.sjms.jms;
 
 /**
- * TODO Add Class documentation for SessionAcknowledgementType
+ * Session acknowledge enum keys
  * 
  */
 public enum SessionAcknowledgementType {
